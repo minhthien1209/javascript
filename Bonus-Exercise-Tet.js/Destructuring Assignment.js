d80dbaf89383f479d3f831a89577dac35d9d71f7
minhthien1209 <tranminhthien120903@gmail.com>
2024-02-18 12:25:44 +0700

    hi

---
+++ Bonus-Exercise-Tet.js/Destructuring Assignment.js
@@ -0,0 +1,2 @@
+import { action, service } from 'lib';
+const { form, error, entity, controller, component } = this.props
---
+++ Bonus-Exercise-Tet.js/arrow function.js
@@ -0,0 +1,13 @@
+sayHello = name => console.log('Hello', name);
+setTimeout(() => console.log('Loaded'), 2000);
+List.forEach(item => console.log(item));
+
+
+
+
+//Implicit return (ẩn return đi bằng arrow function)
+getValue = ratio => ratio * 6.9
+
+
+
+
