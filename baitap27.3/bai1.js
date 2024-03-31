1af57c82246823fba4ba0eb4d6eb69b386e34e25
minhthien1209 <tranminhthien120903@gmail.com>
2024-03-31 12:47:03 +0700

    hello

---
+++ baitap27.3/bai1.js
@@ -0,0 +1,21 @@
+function getPhoneList(apiUrl) {
+    return fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Yêu cầu không thành công. Mã trạng thái: ' + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.error('Đã xảy ra lỗi:', error);
+        });
+}
+
+const apiUrl = 'https://dummyjson.com/products';
+getPhoneList(apiUrl)
+    .then(phone => {
+        console.log('Danh sách điện thoại:', phone);
+    });
